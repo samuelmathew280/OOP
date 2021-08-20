778ab5432e67dcf837b78a208076c8f669090425
samuelmathew280 <78203124+samuelmathew280@users.noreply.github.com>
2021-08-20 14:20:54 +0530

    Create Python Keywords Practice.py

---
+++ Assignment 2/Python Keywords Practice.py
@@ -0,0 +1,24 @@
+#Python Keywords and Identifiers
+abc = 23
+#Python Statements
+sum = (2 + 34 + 
+12 +23 + 2
++ 10)
+dictionary = {'cars' : ['Honda', 'Hyundai', 'Ford'],
+              'color' : ['Red', 'Blue', 'Green']}
+j = 1; k = 2; i = 3
+
+#Python Indentation
+for z in range (1,100):
+    print(z/10)
+    if z%2 == 0:
+        pass
+
+#<--Python Comments
+
+#Docstrings
+def mul(a,b):
+    '''Function to multiply two numbers'''
+    return a*b
+
+print(mul.__doc__)
